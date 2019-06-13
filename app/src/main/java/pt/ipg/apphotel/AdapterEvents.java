e08b6c391ecf49097be68912b5c30b0b1a4705aa
miguelpereira25 <48351052+miguelpereira25@users.noreply.github.com>
2019-06-13 23:27:22 +0100

    Can't do anything else.

---
+++ app/src/main/java/pt/ipg/apphotel/AdapterEvents.java
@@ -0,0 +1,29 @@
+package pt.ipg.apphotel;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdapterEvents extends RecyclerView.Adapter<AdapterEvents> {
+    private Cursor cursor;
+    private Context context;
+
+    public AdapterEvents(Context context) {
+        this.context = context;
+    }
+
+    public void setCursor(Cursor cursor) {
+        if (this.cursor != cursor) {
+            this.cursor = cursor;
+            notifyDataSetChanged();
+        }
+    }
+
+}
